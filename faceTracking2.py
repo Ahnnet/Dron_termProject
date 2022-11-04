25f36fee9f481afd913c18cb1c94d5248e9131df
Ahnnet <drj9802@gmail.com>
2022-11-05 02:50:13 +0900

    comments

--- faceTracking2.py
+++ faceTracking2.py
@@ -1,7 +1,6 @@
 #### final code ####
 
 import cv2
-import numpy as np
 from djitellopy import tello
 import time
 import random
@@ -21,17 +20,18 @@ pid = [0.4, 0.4, 0]
 pError = 0
 findcount = 0
 
-dronDir = True # 드론이 제시하는 방향. True: left / False: right
-result = True
-# dron left/right 결정
+# Drons direction predict. -> True: left / False: right
+dronDir = True
 num = random.randint(0,1)
 print("00000000000000000000000000000000000000000000000000000000000000000")
 print("random: "+str(num))
 print("00000000000000000000000000000000000000000000000000000000000000000")
 if(num==1): dronDir = True
 else: dronDir = False
+
 flagbit = 0
 
+# detect frontal face
 def findFace(img):
     faceCascade = cv2.CascadeClassifier("E:\Dron_termProject\Dron_termProject\opencv-4.x\data\haarcascades\haarcascade_frontalface_default.xml")
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -56,6 +56,7 @@ def findFace(img):
     else:
         return img, [[0, 0], 0], detect
 
+# detect size face
 def findSideFace(img):
     faceCascade = cv2.CascadeClassifier("E:\Dron_termProject\Dron_termProject\opencv-4.x\data\haarcascades\haarcascade_profileface.xml")
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -79,7 +80,7 @@ def findSideFace(img):
         print("right face detected")
         return False
 
-
+# "charm charm charm" motion
 def detected():
     #왼쪽 오른쪽으로 왔다 갔다
     me.move_left(20)
@@ -89,12 +90,14 @@ def detected():
     # me.move_left(40)
     # me.move_right(20)
 
+# plyaer win. -> take commemorative photo
 def playerWin():
     print("player Win")
     me.move_up(40)
     me.move_back(60)
     return 0
 
+# dron win. -> annoying movement
 def dronWin():
     print("drone Win")
     # me.flip("b")
@@ -111,16 +114,17 @@ while True:
     img = me.get_frame_read().frame
     img = cv2.resize(img, (w, h))
     img, info, detect = findFace(img)
-    if(detect == True): findcount += 1
+    if(detect == True): findcount += 1 # if the frontal face detected, findcount++
     print(findcount)
     
     # after detected
     if(flagbit==1):
-        #left / right flip
+        # left
         if(num==1): 
             time.sleep(2.0)
             me.flip('l')
             me.move_right(20)
+        # right
         elif(num==0): 
             time.sleep(2.0)
             me.flip('r')
